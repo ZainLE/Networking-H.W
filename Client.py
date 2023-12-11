6c8259d2c0d66a44b0daabc083a35f8f1485fcc0
ZainLatif7164 <latifzain40@gmail.com>
2023-12-12 00:37:10 +0100

    Final Commit.

--- Client.py
+++ Client.py
@@ -1,17 +1,35 @@
 import socket
 
+socket_one = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = 33123
+server_address = '127.0.0.1'
+
+socket_one.connect((server_address, port))
+
+client_name=input("What is your name? ")
+
+server_name = socket_one.recv(4096).decode()
+print(f"Connected with {server_name}")
+
+
+socket_one.send(client_name.encode())
+
+
 while True:
     socket_one = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    
     port = 33123
     server_address = '127.0.0.1'
 
     socket_one.connect((server_address, port))
     request=input("Type your message here: ")
 
-    socket_one.send(request.encode())
+    socket_one.send(f"{client_name} : {request}".encode())
 
     buffer_size = 4096
     result = socket_one.recv(buffer_size)
 
+    if request.lower() == "exit" or "stop":
+        socket_one.send("exit".encode())
+        break
     print(result.decode())
--- Server.py
+++ Server.py
@@ -5,23 +5,39 @@ socket_one = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 33123
 
 socket_one.bind(("0.0.0.0", port))
-socket_one.listen(1)
+
+socket_one.listen(10)
+
+# name=input("What is your name? ")
+print("Connect from client to continue.")
+
+connected_socket, addr = socket_one.accept()
+
+server_name=input("What is your name? ")
+
+connected_socket.send(server_name.encode())
+
+client_name = connected_socket.recv(4096).decode()
+
+print(f"Connected with {client_name}")
+
+
 
 while True:
     
+    
     connected_socket, addr = socket_one.accept()
-    print("Connection from: " + str(addr))
-
+    
     received_data = connected_socket.recv(4096)
     print(received_data.decode())
     
     user_input=input("Type your message here: ")
     
-    connected_socket.send(user_input.encode())
-    connected_socket.close()
+    connected_socket.send(f"{server_name} : {user_input}".encode())
+    
 
-    if "stop" in received_data.decode():
+    if user_input.lower() == "exit" or "stop":
+        connected_socket.send("exit".encode())
         break
-
+    
 socket_one.close()
-
